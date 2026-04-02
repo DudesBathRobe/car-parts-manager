971007ef69ce7bf2f0c4939a2f1c77009df14421
Mike <14pax88@gmail.com>
2026-04-02 15:13:38 +0300

    Add show all items feature

--- catalog_manager.py
+++ catalog_manager.py
@@ -40,24 +40,58 @@ def add_to_db(item):
     conn.close()
     print(f"--- [Успех]: {item.title} ({item.art}) добавлен в базу! ---")
 
+def show_all_items():
+    conn = sqlite3.connect('accessories.db')
+    cursor = conn.cursor()
+
+    # Выбираем все данные из таблицы
+    cursor.execute('SELECT art, title, car_model, price FROM stock')
+    rows = cursor.fetchall() # fetchall() возвращает список кортежей
+
+    if not rows:
+        print("\n--- База данных пока пуста ---")
+    else:
+        print("\n--- СПИСОК ВСЕХ ТОВАРОВ ---")
+        for row in rows:
+            # Превращаем данные из БД обратно в объект нашего класса (ООП в действии!)
+            item = Accessory(row[0], row[1], row[2], row[3])
+            print(item)
+
+    conn.close()
+
 # --- ЧАСТЬ 3: ГЛАВНЫЙ ЦИКЛ ---
 if __name__ == "__main__":
     init_db()
     print("Добро пожаловать в систему учета запчастей!")
 
     while True:
-        art = input("\nВведите артикул товара (или 'exit' для выхода): ")
-        if art.lower() == 'exit':
+        print("\nМеню:")
+        print("1. Добавить новый товар")
+        print("2. Показать все товары")
+        print("3. Выход (exit)")
+
+        choice = input("Выберите действие: ")
+
+        if choice == '1':
+            art = input("\nВведите артикул товара (или 'exit' для выхода): ")
+            if art.lower() == 'exit':
+                break
+            name = input("Введите название товара: ")
+            car = input("Для какой модели авто (напр. Haval F7x): ")
+            price = float(input("Цена: "))
+
+            # Создаем объект нашего класса
+            new_item = Accessory(art, name, car, price)
+
+            # Сохраняем его в базу данных
+            add_to_db(new_item)
+
+        elif choice == '2':
+            show_all_items()
+
+        elif choice == '3' or choice.lower() == 'exit':
+            print("До встречи!")
             break
-
-        name = input("Введите название товара: ")
-        car = input("Для какой модели авто (напр. Haval F7x): ")
-        price = float(input("Цена: "))
-
-        # Создаем объект нашего класса
-        new_item = Accessory(art, name, car, price)
-
-        # Сохраняем его в базу данных
-        add_to_db(new_item)
-
+        else:
+            print("Неверный ввод, попробуйте еще раз.")
 
